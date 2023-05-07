6fc8219c98a925056db122d29382825ed22068cb
yrgenius <yrgenius80@gmail.com>
2023-05-07 12:01:49 +0900

    feat: button start, score

--- main.js
+++ main.js
@@ -1,20 +1,62 @@
 const dino = document.querySelector('#dino');
 const cactus = document.querySelector('#cactus');
+const startBtn = document.querySelector('.btn__start');
+const scoreOut = document.querySelector('.score');
+let isStart = false;
+let score = 0;
 
+startBtn.addEventListener('click', start);
 document.addEventListener('keydown', jump);
 
-function jump() {
-    if (dino.classList != 'jump') dino.classList.add('jump');
-    setTimeout(remove, 500);
+//TODO: разобраться с фокусом на кнопке 
+function start() {
+    isStart
+        ? stopAnimation()
+        : startAnimation();
+
+    let isAlive = setInterval(function () {
+        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
+
+        if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+            stopAnimation();
+        }
+
+        //TODO: разобраться с подсчетом очков 
+        if (cactusLeft === 1) {
+            score += 150;
+            scoreOut.textContent = score;
+        }
+    }, 10);
 }
 
-function remove() {
+
+
+function jump() {
+    if (event.code === 'Space') {
+        if (dino.classList != 'jump') dino.classList.add('jump');
+        setTimeout(endJump, 500);
+    }
+}
+
+function endJump() {
     dino.classList.remove('jump');
 }
 
-let isAlive = setInterval(function () {
-    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
+function startAnimation() {
+    cactus.classList.add('active');
+    startBtn.textContent = 'Stop';
+    isStart = !isStart;
+}
 
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) alert('GAME OVER');
-}, 10);
+function stopAnimation() {
+    cactus.classList.remove('active');
+    startBtn.textContent = 'Start';
+    isStart = !isStart;
+    score = 0;
+    scoreOut.textContent = score;
+    clearInterval(isAlive);
+}
+
+
+
